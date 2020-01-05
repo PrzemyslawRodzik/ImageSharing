d10c32eecc7aa74c7b978c4a7f00731268478853
PrzemyslawR <51419578+PrzemyslawRodzik@users.noreply.github.com>
2020-01-05 14:41:45 +0100

    PostFactory

--- ImageSharing/database/factories/PostFactory.php
+++ ImageSharing/database/factories/PostFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        
+        'caption' => $faker->title,
+        'image' => 'uploads/'.$faker->image('public/storage/uploads',1200,1200, null, false),
+
     ];
 });
