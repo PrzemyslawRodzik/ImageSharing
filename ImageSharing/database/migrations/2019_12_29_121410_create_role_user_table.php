fc6a9e62fcf6ad136b35bcde7c4db005dc146f2e
PrzemyslawR <51419578+PrzemyslawRodzik@users.noreply.github.com>
2020-01-07 22:28:33 +0100

    Added changes to .gitignore file

--- ImageSharing/database/migrations/2019_12_29_121410_create_role_user_table.php
+++ ImageSharing/database/migrations/2019_12_29_121410_create_role_user_table.php
@@ -46,7 +46,7 @@ class CreateRoleUserTable extends Migration
 
 
 
-        Artisan::call( 'db:seed', [
+      /*  Artisan::call( 'db:seed', [
                 '--class' => 'RolesTableSeeder',
                 '--force' => true ]
         );
@@ -55,7 +55,7 @@ class CreateRoleUserTable extends Migration
                 '--force' => true ]
         );
 
-        Artisan::call('storage:link', [] );
+        Artisan::call('storage:link', [] );*/
 
 
     }
